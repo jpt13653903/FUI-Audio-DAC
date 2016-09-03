2bf023fe70502d5b5105c807cb28cf4394d4d343
John-Philip Taylor <jpt13653903@gmail.com>
2016-09-03 18:46:14 +0200

    Added license information

--- Quartus/USB Amplifier/USB/USB_Audio.v
+++ Quartus/USB Amplifier/USB/USB_Audio.v
@@ -1,3 +1,23 @@
+//==============================================================================
+// Copyright (C) John-Philip Taylor
+// jpt13653903@gmail.com
+//
+// This file is part of a library
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+//==============================================================================
+
 // For all arrays, 0 => Left, 1 => Right
 //------------------------------------------------------------------------------
 
@@ -85,9 +105,9 @@ always @(posedge Clk) begin
    
  end else begin
   if(FIFO_Ready) begin
-   if(FIFO_Length < 8'h20) FIFO_Ready <= 1'b0;
+   if(FIFO_Length == 8'h20) FIFO_Ready <= 1'b0;
   end else begin
-   if(FIFO_Length > 8'h80) FIFO_Ready <= 1'b1;
+   if(FIFO_Length == 8'h80) FIFO_Ready <= 1'b1;
   end
 
   if(FIFO_Ready) begin
