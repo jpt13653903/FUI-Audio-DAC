0b9e32ec767e99dd14bfcbed4afe60ad54a77f1c
John-Philip Taylor <jpt13653903@gmail.com>
2016-10-01 16:48:49 +0200

    Remote is fully functional

--- MPLAB/USB Audio Remote.X/Source/main.c
+++ MPLAB/USB Audio Remote.X/Source/main.c
@@ -1,41 +1,165 @@
+//==============================================================================
+// Copyright (C) John-Philip Taylor
+// jpt13653903@gmail.com
+//
+// This file is part of FUI Audio DAC
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+//==============================================================================
+
 #include "config.h"
 #include "global.h"
 //------------------------------------------------------------------------------
 
-bool Active = false;
-word Count  = 0;
+bool Active      = false;
+byte ActiveCount = 0;
+byte Volume      = 0;
+//------------------------------------------------------------------------------
+
+void MakeActive(){
+ Active      = true;
+ ActiveCount = 0;
+
+ Interrupt        = 1;
+ TRISC            = 0xCF;
+ TRISAbits.TRISA2 = 0;
+
+ INTCONbits.INTE = 0;
+ INTCONbits.T0IE = 1;
+}
+//------------------------------------------------------------------------------
+
+void OnVolumeChange(){
+ static byte Prev = 0;
+ 
+ byte Next = (Volume2 << 1) | Volume1;
+
+ switch((Prev << 4) | Next){
+  case 0x01:
+  case 0x13:
+  case 0x32:
+  case 0x20:
+   Volume++;
+   break;
+
+  case 0x02:
+  case 0x10:
+  case 0x31:
+  case 0x23:
+   Volume--;
+   break;
+
+  default:
+   break;
+ }
+
+ Prev = Next;
+}
+//------------------------------------------------------------------------------
+
+byte Parity(word Data){
+ byte Result;
+
+ Result = (Data   & 0xFF) ^ (Data   >> 8);
+ Result = (Result & 0x0F) ^ (Result >> 4);
+ Result = (Result & 0x03) ^ (Result >> 2);
+ Result = (Result & 0x01) ^ (Result >> 1);
+ 
+ // Has to be odd parity to ensure that the final edge is falling
+ return (~Result) & 1;
+}
+//------------------------------------------------------------------------------
+
+void OnTimer(){
+ typedef enum STATE_TAG{
+  Idle,
+  SendEdge,
+  SendData
+ } STATE;
+ 
+ static STATE State = Idle;
+ static byte  Count = 0;
+ static word  Data;
+
+ switch(State){
+  case Idle:
+   Count++;
+   if(Count == 3){
+    Data = Buttons;
+    Data = (Data << 8) | Volume;
+    Data = (Data << 4) | Parity(Data);
+
+    Count = 0;
+    State = SendEdge;
+   }
+   break;
+
+  case SendEdge:
+   Tx = !Tx;
+
+   if(Count == 16){
+    ActiveCount++;
+    if(ActiveCount > 25) Active = false;
+
+    Count = 0;
+    State = Idle;
+
+   }else{
+    State = SendData;
+   }
+   break;
+
+  case SendData:
+   if(Data & 0x8000) Tx = !Tx;
+   Data = Data << 1;
+
+   Count++;
+   State = SendEdge;
+
+   break;
+
+  default:
+   break;
+ }
+}
 //------------------------------------------------------------------------------
 
 interrupt void OnInterrupt(){
- if(INTCONbits.T0IF){
-  if(Active){
-   Count++;
-   if(Count > 1000) Active = false;
-
-  }else{
-   Active = true;
-   Count  = 0;
-  }
-  LED = Active;
-
-  NOP();
-  NOP();
-
-  Tx = !Tx;
-  INTCONbits.T0IF = 0;
- }
-
+ // Interrupt-on-change, port A
  if(INTCONbits.RAIF){
-  Active = true;
-  Count  = 0;
-  byte Temp = PORTA;
+  MakeActive    ();
   INTCONbits.RAIF = 0;
  }
 
+ // Interrupt on falling edge of INT pin
  if(INTCONbits.INTF){
-  Active = true;
-  Count  = 0;
+  MakeActive();
   INTCONbits.INTF = 0;
+ }
+
+ // Interrupt on Timer 0 overflow
+ if(INTCONbits.T0IF){
+  if(Buttons) ActiveCount = 0;
+
+  OnVolumeChange(); // Uses slow-sampling to de-bounce: sample every 500 μs
+
+  OnTimer();
+
+  INTCONbits.T0IE = Active;
+  LED             = Active;
+
+  INTCONbits.T0IF = 0;
  }
 }
 //------------------------------------------------------------------------------
@@ -52,20 +176,20 @@ void main(){
  ANSEL = 0; // Disable analogue functionality
  WPUA  = 0; // Disable weak pull-up on port A
 
- PORTC = 0;
- TRISA = 0xFF;
- TRISC = 0xC0;
-
- IOCA = 0x30; // Interrupt-on-change on A4 and A5
-
- INTCONbits.RAIE = 1; // Enable port A interrupt-on-change
- INTCONbits.INTE = 1; // Enable INT pin interrupt
-
- INTCONbits.T0IE = 1; // Enable interrupt on Timer 0
- INTCONbits.GIE  = 1; // Enable global interrupts
+ IOCA            = 0x30; // Interrupt-on-change on A4 and A5
+ INTCONbits.RAIE = 1;    // Enable port A interrupt-on-change
 
  while(1){
-  if(!Active) SLEEP();
+  if(!Active){
+   INTCONbits.GIE = 0;
+    PORTC = 0;
+    TRISA = 0xFF;
+    TRISC = 0xC0;
+    INTCONbits.INTF = 0;
+    INTCONbits.INTE = 1;
+   INTCONbits.GIE = 1;
+   SLEEP();
+  }
  }
 }
 //------------------------------------------------------------------------------
