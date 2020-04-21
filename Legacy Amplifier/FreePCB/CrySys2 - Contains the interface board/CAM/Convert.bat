e6cf5b2b458178cf473c9b0e76e91ea371c79c38
John-Philip Taylor <John-PhilipT@reutech.co.za>
2020-04-21 15:07:49 +0200

    Added more PCBs from the prototype system and moved to Legacy folder

---
+++ Legacy Amplifier/FreePCB/CrySys2 - Contains the interface board/CAM/Convert.bat
@@ -0,0 +1,4 @@
+echo off
+
+"W:\Projects\12\1204 - Gerber2pdf\CodeBlocks\bin\Gerber2pdf" "-output=Output" top_silk.grb bottom_silk.grb top_copper.grb inner_copper_1.grb inner_copper_2.grb bottom_copper.grb top_solder_mask.grb bottom_solder_mask.grb board_outline.grb
+
